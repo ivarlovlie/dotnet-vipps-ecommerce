89d7b626822d65d0f54dea8b3cd9fefe292da3c5
ivarlovlie <git@ivarlovlie.no>
2021-03-31 22:00:58 +0200

    Expose configuration on IVippsEcommerceService

--- src/IOL.VippsEcommerce/IVippsEcommerceService.cs
+++ src/IOL.VippsEcommerce/IVippsEcommerceService.cs
@@ -1,11 +1,14 @@
 using System.Threading;
 using System.Threading.Tasks;
+using IOL.VippsEcommerce.Models;
 using IOL.VippsEcommerce.Models.Api;
 
 namespace IOL.VippsEcommerce
 {
 	public interface IVippsEcommerceService
 	{
+		public VippsConfiguration Configuration { get; }
+
 		public Task<VippsInitiatePaymentResponse> InitiatePaymentAsync(
 			VippsInitiatePaymentRequest payload,
 			CancellationToken ct = default

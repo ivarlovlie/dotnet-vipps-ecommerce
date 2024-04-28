2cdb9549dda574fc91f44aecefe6c4620257f050
ivar <i@oiee.no>
2024-04-28 22:32:17 +0200

    Remove unsupported fields

--- src/IOL.VippsEcommerce/Models/Api/TMerchantInfo.cs
+++ src/IOL.VippsEcommerce/Models/Api/TMerchantInfo.cs
@@ -6,25 +6,11 @@ namespace IOL.VippsEcommerce.Models.Api;
 public class TMerchantInfo
 {
 	/// <summary>
-	/// Authorization token that the merchant could share to make callbacks more secure. If provided this token will be returned as an &#x60;Authorization&#x60; header for our callbacks. This includes shipping details and callback.
-	/// </summary>
-	/// <value>Authorization token that the merchant could share to make callbacks more secure. If provided this token will be returned as an &#x60;Authorization&#x60; header for our callbacks. This includes shipping details and callback.</value>
-	//[JsonPropertyName("authToken")]
-	//public string AuthToken { get; set; }
-
-	/// <summary>
 	/// This is an URL for Vipps to call at the merchant&#x27;s server to provide updated information about the order after the payment request. Domain name and context path should be provided by merchant as the value for this parameter. Vipps will add &#x60;/v2/payments/{orderId}&#x60; to the end or this URL. URLs passed to Vipps must validate with the Apache Commons [UrlValidator](https://commons.apache.org/proper/commons-validator/apidocs/org/apache/commons/validator/routines/UrlValidator.html). We don&#x27;t send requests to all ports, so to be safe use common ports such as: 80, 443, 8080.
 	/// </summary>
 	/// <value>This is an URL for Vipps to call at the merchant&#x27;s server to provide updated information about the order after the payment request. Domain name and context path should be provided by merchant as the value for this parameter. Vipps will add &#x60;/v2/payments/{orderId}&#x60; to the end or this URL. URLs passed to Vipps must validate with the Apache Commons [UrlValidator](https://commons.apache.org/proper/commons-validator/apidocs/org/apache/commons/validator/routines/UrlValidator.html). We don&#x27;t send requests to all ports, so to be safe use common ports such as: 80, 443, 8080.</value>
 	[JsonPropertyName("callbackPrefix")]
 	public string CallbackPrefix { get; set; }
-
-	/// <summary>
-	/// Required for Vipps Hurtigkasse (express checkout) payments. This callback URL will be used by Vipps to inform the merchant that the user has revoked his/her consent: This Vipps user does do not want the merchant to store or use his/her personal information anymore. Required by GDPR. Vipps will add &#x60;/v2/consents/{userId}&#x60; to the end or this URL. URLs passed to Vipps should be URL-encoded, and must validate with the Apache Commons [UrlValidator](https://commons.apache.org/proper/commons-validator/apidocs/org/apache/commons/validator/routines/UrlValidator.html). We don&#x27;t send requests to all ports, so to be safe use common ports such as: 80, 443, 8080.
-	/// </summary>
-	/// <value>Required for Vipps Hurtigkasse (express checkout) payments. This callback URL will be used by Vipps to inform the merchant that the user has revoked his/her consent: This Vipps user does do not want the merchant to store or use his/her personal information anymore. Required by GDPR. Vipps will add &#x60;/v2/consents/{userId}&#x60; to the end or this URL. URLs passed to Vipps should be URL-encoded, and must validate with the Apache Commons [UrlValidator](https://commons.apache.org/proper/commons-validator/apidocs/org/apache/commons/validator/routines/UrlValidator.html). We don&#x27;t send requests to all ports, so to be safe use common ports such as: 80, 443, 8080.</value>
-	//[JsonPropertyName("consentRemovalPrefix")]
-	//public string ConsentRemovalPrefix { get; set; }
 
 	/// <summary>
 	/// Vipps will use the fallBack URL to redirect the Vipps user to the merchant’s confirmation page once the payment is completed in Vipps. This is normally the “success page”, although the “fallback” name is ambiguous (the same URL is also used if payment was not successful). In other words: This is the URL Vipps sends the Vipps user back to. URLs passed to Vipps must validate with the Apache Commons [UrlValidator](https://commons.apache.org/proper/commons-validator/apidocs/org/apache/commons/validator/routines/UrlValidator.html).
--- src/IOL.VippsEcommerce/Models/Api/VippsPaymentInitiationCallbackResponse.cs
+++ src/IOL.VippsEcommerce/Models/Api/VippsPaymentInitiationCallbackResponse.cs
@@ -5,114 +5,104 @@ namespace IOL.VippsEcommerce.Models.Api;
 
 public class VippsPaymentInitiationCallbackResponse
 {
-	[JsonPropertyName("merchantSerialNumber")]
-	public string MerchantSerialNumber { get; set; }
+    [JsonPropertyName("merchantSerialNumber")]
+    public string MerchantSerialNumber { get; set; }
 
-	[JsonPropertyName("orderId")]
-	public string OrderId { get; set; }
+    [JsonPropertyName("orderId")]
+    public string OrderId { get; set; }
 
-	// [JsonPropertyName("shippingDetails")]
-	// public TShippingDetails? ShippingDetails { get; set; }
+    [JsonPropertyName("transactionInfo")]
+    public TTransactionInfo TransactionInfo { get; set; }
 
-	[JsonPropertyName("transactionInfo")]
-	public TTransactionInfo TransactionInfo { get; set; }
+    public class TErrorInfo
+    {
+        [JsonPropertyName("errorGroup")]
+        public string ErrorGroup { get; set; }
 
-	// [JsonPropertyName("userDetails")]
-	// public UserDetails? UserDetails { get; set; }
-	//
-	// [JsonPropertyName("errorInfo")]
-	// public TErrorInfo? ErrorInfo { get; set; }
+        [JsonPropertyName("errorCode")]
+        public string ErrorCode { get; set; }
 
+        [JsonPropertyName("errorMessage")]
+        public string ErrorMessage { get; set; }
 
-	public class TErrorInfo
-	{
-		[JsonPropertyName("errorGroup")]
-		public string ErrorGroup { get; set; }
+        [JsonPropertyName("contextId")]
+        public Guid ContextId { get; set; }
+    }
 
-		[JsonPropertyName("errorCode")]
-		public string ErrorCode { get; set; }
+    public class TShippingDetails
+    {
+        [JsonPropertyName("address")]
+        public TAddress Address { get; set; }
 
-		[JsonPropertyName("errorMessage")]
-		public string ErrorMessage { get; set; }
+        [JsonPropertyName("shippingCost")]
+        public int ShippingCost { get; set; }
 
-		[JsonPropertyName("contextId")]
-		public Guid ContextId { get; set; }
-	}
+        [JsonPropertyName("shippingMethod")]
+        public string ShippingMethod { get; set; }
 
-	public class TShippingDetails
-	{
-		[JsonPropertyName("address")]
-		public TAddress Address { get; set; }
+        [JsonPropertyName("shippingMethodId")]
+        public string ShippingMethodId { get; set; }
+    }
 
-		[JsonPropertyName("shippingCost")]
-		public int ShippingCost { get; set; }
+    public class TAddress
+    {
+        [JsonPropertyName("addressLine1")]
+        public string AddressLine1 { get; set; }
 
-		[JsonPropertyName("shippingMethod")]
-		public string ShippingMethod { get; set; }
+        [JsonPropertyName("addressLine2")]
+        public string AddressLine2 { get; set; }
 
-		[JsonPropertyName("shippingMethodId")]
-		public string ShippingMethodId { get; set; }
-	}
+        [JsonPropertyName("city")]
+        public string City { get; set; }
 
-	public class TAddress
-	{
-		[JsonPropertyName("addressLine1")]
-		public string AddressLine1 { get; set; }
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
 
-		[JsonPropertyName("addressLine2")]
-		public string AddressLine2 { get; set; }
+        [JsonPropertyName("zipCode")]
+        public string ZipCode { get; set; }
+    }
 
-		[JsonPropertyName("city")]
-		public string City { get; set; }
+    public class TTransactionInfo
+    {
+        [JsonPropertyName("amount")]
+        public int Amount { get; set; }
 
-		[JsonPropertyName("country")]
-		public string Country { get; set; }
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
 
-		[JsonPropertyName("zipCode")]
-		public string ZipCode { get; set; }
-	}
+        public ETransactionStatus StatusEnum() => Enum.Parse<ETransactionStatus>(Status);
 
-	public class TTransactionInfo
-	{
-		[JsonPropertyName("amount")]
-		public int Amount { get; set; }
+        [JsonPropertyName("timeStamp")]
+        public DateTime TimeStamp { get; set; }
 
-		[JsonPropertyName("status")]
-		public string Status { get; set; }
+        [JsonPropertyName("transactionId")]
+        public string TransactionId { get; set; }
+    }
 
-		public ETransactionStatus StatusEnum() => Enum.Parse<ETransactionStatus>(Status);
+    public class TUserDetails
+    {
+        [JsonPropertyName("bankIdVerified")]
+        public string BankIdVerified { get; set; }
 
-		[JsonPropertyName("timeStamp")]
-		public DateTime TimeStamp { get; set; }
+        [JsonPropertyName("dateOfBirth")]
+        public string DateOfBirth { get; set; }
 
-		[JsonPropertyName("transactionId")]
-		public string TransactionId { get; set; }
-	}
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
 
-	public class TUserDetails
-	{
-		[JsonPropertyName("bankIdVerified")]
-		public string BankIdVerified { get; set; }
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
 
-		[JsonPropertyName("dateOfBirth")]
-		public string DateOfBirth { get; set; }
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
 
-		[JsonPropertyName("email")]
-		public string Email { get; set; }
+        [JsonPropertyName("mobileNumber")]
+        public string MobileNumber { get; set; }
 
-		[JsonPropertyName("firstName")]
-		public string FirstName { get; set; }
+        [JsonPropertyName("ssn")]
+        public string Ssn { get; set; }
 
-		[JsonPropertyName("lastName")]
-		public string LastName { get; set; }
-
-		[JsonPropertyName("mobileNumber")]
-		public string MobileNumber { get; set; }
-
-		[JsonPropertyName("ssn")]
-		public string Ssn { get; set; }
-
-		[JsonPropertyName("userId")]
-		public string UserId { get; set; }
-	}
+        [JsonPropertyName("userId")]
+        public string UserId { get; set; }
+    }
 }
